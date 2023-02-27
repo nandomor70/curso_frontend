6cd5add8d0d0ed80e18932f777d5f62002f46a53
nandomor70 <126270165+nandomor70@users.noreply.github.com>
2023-02-27 12:31:18 -0300

    Update datos.js

--- JS/datos.js
+++ JS/datos.js
@@ -4,11 +4,13 @@ const foto = document.querySelector('#foto');
 const nombre = document.querySelector('#nombre');
 const apellido = document.querySelector('#apellido');
 const correo = document.querySelector('#correo');
+const direccion = document.querySelector('#direccion');
 const telefono = document.querySelector('#telefono');
 const calle = document.querySelector('#calle');
 const nro = document.querySelector('#nro');
 const ciudad = document.querySelector('#ciudad');
 const pais = document.querySelector('#pais');
+
 
 //funcion
 const nuevousuario = async () => {
@@ -16,7 +18,7 @@ const nuevousuario = async () => {
   const respuesta = await fetch(url);
   const {results} = await respuesta.json();
   const datos = results [0];
-  console.log(datos);
+  //console.log(datos);
   foto.src = datos.picture.medium;
   nombre.textContent = datos.name.first;
   apellido.textContent = datos.name.last;
@@ -26,6 +28,11 @@ const nuevousuario = async () => {
   nro.textContent = datos.location.street.number;
   ciudad.textContent = datos.location.city;
   pais.textContent = datos.location.country;
+  var link = "mailto:"+ datos.email;
+  var element = document.createElement("a");
+  element.setAttribute("href", link);
+  element.innerHTML = "Enviar correo";
+  document.getElementById("tab4").appendChild(element);
 }
 
 //recarga pagina
